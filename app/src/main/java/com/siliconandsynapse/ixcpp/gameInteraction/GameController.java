6c360d5b2d5e00d9345ef632238232887a3bbadc
andrew <shawa80@yahoo.com>
2024-05-25 11:03:11 -0500

    adding player card event

---
+++ app/src/main/java/com/siliconandsynapse/ixcpp/gameInteraction/GameController.java
@@ -0,0 +1,9 @@
+//package com.siliconandsynapse.ixcpp.gameInteraction;
+//
+//import com.siliconandsynapse.net.ixtunnel.IxManager;
+//
+//public interface GameController
+//{
+//	public void startGame(IxManager returnTunnel, String gameName, String gameType);
+//	public void quitGame(GameInstance game);
+//}
