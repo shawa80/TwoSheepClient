976bc37d1c47a595fe76382d34d7fdf02595e80b
andrew <shawa80@yahoo.com>
2024-06-20 21:18:13 -0500

    version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.siliconandsynapse.aclient"
-        minSdk = 32
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
