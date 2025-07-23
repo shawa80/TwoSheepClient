249e3e48f533994b15a63834ee655ff297af8459
andrew <shawa80@yahoo.com>
2025-07-22 19:20:43 -0500

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.siliconandsynapse.aclient"
         minSdk = 34
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.0.6"
+        versionCode = 7
+        versionName = "1.0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
